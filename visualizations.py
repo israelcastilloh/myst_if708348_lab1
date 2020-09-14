67d357f4c5f3fea547ca5a00f3ff7323fcf3c4fb
israelcastilloh <castillo.israelh@gmail.com>
2020-09-13 22:49:34 -0500

    deploy

--- visualizations.py
+++ visualizations.py
@@ -9,6 +9,7 @@
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 from main import *
+from data import *
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash
